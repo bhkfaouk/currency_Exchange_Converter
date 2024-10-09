8dc8012280fa36bd5a045d33ff8bed740588793d
bhkfarouk <faroukbouhaka@gmail.com>
2024-10-09 18:09:46 +0200

    handle unnecessery files

--- src/main/java/bhk/microservices/currency_exchange_service/Controller/CurrencyExchangeController.java
+++ src/main/java/bhk/microservices/currency_exchange_service/Controller/CurrencyExchangeController.java
@@ -3,9 +3,7 @@ package bhk.microservices.currency_exchange_service.Controller;
 import bhk.microservices.currency_exchange_service.Models.CurrencyRequest;
 import bhk.microservices.currency_exchange_service.Models.CurrencyResponse;
 
-import bhk.microservices.currency_exchange_service.microcervicesConfig.currencyExchangeServiceProxy;
 import bhk.microservices.currency_exchange_service.services.ExchangeService;
-import bhk.microservices.currency_exchange_service.services.ExchangeServiceImpl;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -19,7 +17,7 @@ public class CurrencyExchangeController {
 
     private final ExchangeService exchangeService;
 
-    private final currencyExchangeServiceProxy proxy;
+    //private final currencyExchangeServiceProxy proxy;
 
 @PostMapping("/exchangeValue")
     public ResponseEntity<CurrencyResponse> getExchangeValue(@RequestBody CurrencyRequest currencyRequest) throws IOException {
